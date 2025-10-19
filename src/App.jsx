d83c43e3f54c232e6aad408f2a7be7890a474413
Althafx <althaf.0.2077@gmail.com>
2025-10-19 19:51:41 +0530

    created a home pafe with header component which have a logo and navbar, then a body component with searxh bar and a card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//heaeder
+//-navbar,-logo
+
+//body
+//-card
+
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Header(){
+  return(
+    <div className="headerContainer">
+      <div className="logo">
+        <img className='logo-img' src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg" alt="React Logo" />
+      </div>
+      <div>Althaf's Empire</div>
+      <div className="navItems">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
+function Card(){
+  return(
+    <div className="card">
+      <div className='img'>
+      <img className='hotel-img' src="https://pix10.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?ca=6&ce=1&s=414x232" alt="Placeholder Image" />
+      </div>
+      <h3>Althaf's Hotel</h3>
+      <h4>Kochi Kerala</h4>
+      <h4>20 min</h4>
+      <h4>chinese, thai, indian</h4>
+      <h4>4.9</h4>
+    </div>
+  )
+}
+
+function Body(){
+  return(
+
+    <div className="bodyContainer">
+    <div className='searchbar'>
+      <input type="text" placeholder='Search...' />
+    </div>
+
+    <Card/>
+        <Card/>
+            <Card/>
+                <Card/>
+                    <Card/>
+                        <Card/>
+        <Card/>    <Card/>    <Card/>    <Card/>
+
+
+
+    </div>
+  )
+}
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+     
+      <Header />
+      <Body/>
+    </div>
   )
 }
 
